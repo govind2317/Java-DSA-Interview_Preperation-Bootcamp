a2aad10f0a33011ef2a0af7e8381f611e309b923
Ansalpandey <pandeyansal77@gmail.com>
2024-04-17 23:33:48 +0530

    Code added.

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main{
+    //Class is the way of grouping methods and variables.
+    //public - access modifier
+    public static void main(String[] args) {
+        //Variables
+        //datatype- What kind of data is going to be stored.
+        //int - stores integer values. - 1, 2, 5
+        //float - floating point numbers - 4.396
+        //double - floating point numbers - 42.36896565655656
+        //String-Character sequence - "Ansal Pandey"
+        //char - single character. - 'A'
+
+        //datatype variable_name = value;
+
+        // single-line comments
+
+        /*
+         Multi
+         Line
+         Comments
+         */
+
+        int age = 0;
+        String name = "Govind Dwivedi";
+        float marks = 63.325f;
+        char grade = 'B';
+
+
+
+        System.out.println("My name is:- " + name);
+        System.out.println("My age is:- " + age);
+        System.out.println("My marks is:- " + marks);
+        System.out.println("My grade is:- " + grade);
+    }
+}
